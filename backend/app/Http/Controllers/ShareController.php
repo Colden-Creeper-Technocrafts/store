de620998a3afa173c1b1815251cceb0862a158b2
Raj Thakkar <rgthakkar013@gmail.com>
2026-07-21 22:07:30 +0530

    Social Media link modifications

--- backend/app/Http/Controllers/ShareController.php
+++ backend/app/Http/Controllers/ShareController.php
@@ -2,21 +2,34 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class ShareController extends Controller
 {
     public function product(string $slug): Response
     {
-        $product = Product::where('sku', $slug)->first();
+        // Mirror StorefrontRepository: join default variant → primary image
+        $product = DB::table('products')
+            ->leftJoin('product_variants as pv', function ($join) {
+                $join->on('pv.id', '=', DB::raw(
+                    '(SELECT id FROM product_variants WHERE product_id = products.id ORDER BY is_default DESC, id ASC LIMIT 1)'
+                ));
+            })
+            ->leftJoin('product_images', function ($join) {
+                $join->on('product_images.product_variant_id', '=', 'pv.id')
+                    ->where('product_images.is_primary', true);
+            })
+            ->select(['products.name', 'products.short_description', 'product_images.image'])
+            ->where(function ($q) use ($slug) {
+                $q->where('products.slug', $slug)->orWhere('products.sku', $slug);
+            })
+            ->first();
 
-        $name    = e($product?->name ?? 'Check out this product');
-        $desc    = e($product?->short_description ?? $product?->name ?? '');
-        $rawImg  = $product?->image ?? '';
-        $image   = $rawImg
-            ? (str_starts_with($rawImg, 'http') ? e($rawImg) : e(rtrim(config('app.url'), '/') . '/' . ltrim($rawImg, '/')))
-            : '';
+        $name   = e($product?->name ?? 'Check out this product');
+        $desc   = e($product?->short_description ?? $product?->name ?? '');
+        $rawImg = $product?->image ?? '';
+        $image  = $rawImg ? e(asset('storage/' . ltrim($rawImg, '/'))) : '';
 
         $frontendUrl = rtrim(config('app.frontend_url', config('app.url')), '/');
         $spaUrl      = $frontendUrl . '/product/' . rawurlencode($slug);
@@ -24,7 +37,7 @@ class ShareController extends Controller
         $spaUrlJs    = json_encode($spaUrl);
 
         $ogImageTags = $image
-            ? "  <meta property=\"og:image\" content=\"{$image}\">\n  <meta name=\"twitter:image\" content=\"{$image}\">"
+            ? "  <meta property=\"og:image\" content=\"{$image}\">\n  <meta property=\"og:image:width\" content=\"1200\">\n  <meta name=\"twitter:image\" content=\"{$image}\">"
             : '';
 
         $html = <<<HTML
